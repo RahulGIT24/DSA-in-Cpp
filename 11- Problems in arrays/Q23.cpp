5f812ea4cda23f5e6f9cdf77318f4f2b12ad2ee6
Rahul <rg4005450@gmail.com>
2023-01-11 16:15:28 +0530

    Solved an array Question

--- 11- Problems in arrays/Q23.cpp
+++ 11- Problems in arrays/Q23.cpp
@@ -4,23 +4,33 @@
 using namespace std;
 int main()
 {
+    // Declaring Array
     int array[10] = {2, 3, 4, 5, 6, 7, 12, 6, 90, 67};
-    int ask, pos;
+
+    int ask, pos; // Creating variables
+    
+    // Taking number input from user
     cout << "Enter a number " << endl;
     cin >> ask;
+
+    // Taking position input from user
     cout << "Enter the position where you want to insert" << ask << " " << endl;
     cin>>pos;
 
+    // Printing original array
     cout<<"Original Array"<<endl;
     for (int i = 0; i < 10; i++)
     {
         cout<<array[i]<<" ";
     }
     cout<<endl;
-    cout<<"Updated Array"<<endl;
 
+    // Updating array by adding number to the specific position entered by user
     array[pos-1]=ask;
 
+    cout<<"Updated Array"<<endl;
+
+    //Printing Updated array
     for (int i = 0; i < 11; i++)
     {
         cout<<array[i]<<" ";
